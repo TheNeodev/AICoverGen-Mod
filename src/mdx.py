9828fe0841d3e13ba546253ad58a4b94d6e371b2
Neo <neoforevershog@gmail.com>
2025-01-10 02:15:06 +0700

    Update S

--- src/mdx.py
+++ src/mdx.py
@@ -5,6 +5,7 @@ import queue
 import threading
 import warnings
 
+from audio_separator.separator import Separator
 import librosa
 import numpy as np
 import onnxruntime as ort
@@ -298,13 +299,15 @@ def run_roformer(model_params, output_dir, model_name, filename, exclude_main=Fa
     wave, sr = librosa.load(filename, mono=False, sr=44100)
     base_name = os.path.splitext(os.path.basename(filename))[0]
     
-    roformer_output_format = 'wav'
-    roformer_overlap = 4
-    roformer_segment_size = 256
-    print(f"output_dir: {output_dir}")
-    prompt = f'audio-separator "{filename}" --model_filename {model_name} --output_dir="{output_dir}" --output_format={roformer_output_format} --normalization=0.9 --mdxc_overlap={roformer_overlap} --mdxc_segment_size={roformer_segment_size}'
-    os.system(prompt)
-
+    output_names = {
+        "Vocals": f"{base_name}_vocals",
+        "Instrumental": f"{base_name}_instrumental",
+    }
+    
+    separator = Separator(output_dir=output_dir)
+    separator.load_model(model_filename=model_filename)
+    output_files = separator.separate(filename, output_names)
+    
     vocals_file = f"{base_name}_Vocals.wav"
     instrumental_file = f"{base_name}_Instrumental.wav"
 
@@ -313,14 +316,10 @@ def run_roformer(model_params, output_dir, model_name, filename, exclude_main=Fa
 
     if not exclude_main:
         main_filepath = os.path.join(output_dir, vocals_file)
-        if os.path.exists(os.path.join(output_dir, f"{base_name}_(Vocals)_{model_name.replace('.9755.ckpt', '')}.wav")):
-            os.rename(os.path.join(output_dir, f"{base_name}_(Vocals)_{model_name.replace('.9755.ckpt', '')}.wav"), main_filepath)
-
+        
     if not exclude_inversion:
         invert_filepath = os.path.join(output_dir, instrumental_file)
-        if os.path.exists(os.path.join(output_dir, f"{base_name}_(Instrumental)_{model_name.replace('.9755.ckpt', '')}.wav")):
-            os.rename(os.path.join(output_dir, f"{base_name}_(Instrumental)_{model_name.replace('.9755.ckpt', '')}.wav"), invert_filepath)
-
+        
     if not keep_orig:
         os.remove(filename)
 
